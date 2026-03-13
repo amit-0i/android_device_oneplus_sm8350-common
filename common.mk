8fca650bfaf39feca84565e9de70a73247b6cdee
amit-0i <6329amit@gmail.com>
2026-03-14 01:16:18 +0530

    sm8350-common: add GameBar

--- common.mk
+++ common.mk
@@ -145,6 +145,9 @@ PRODUCT_COPY_FILES += \
 
 $(call soong_config_set,surfaceflinger,udfps_lib,//hardware/oplus:libudfps_extension.oplus)
 
+# GameBar Performance Overlay
+$(call inherit-product, packages/apps/GameBar/gamebar.mk)
+
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_ODM)/etc/gps.conf
