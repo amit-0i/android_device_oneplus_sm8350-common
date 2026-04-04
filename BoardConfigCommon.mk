9357f8916e0ee927cc5ba02068c9e027773d6dd5
amit-0i <6329amit@gmail.com>
2026-04-04 14:18:00 +0530

    sm8350-common: audio: Add support for dolby

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -30,7 +30,7 @@ TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_VARIANT := cortex-a76
 
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a76
@@ -54,6 +54,9 @@ TARGET_BOOTLOADER_BOARD_NAME := lahaina
 
 # Display
 TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE := true
+
+# Dolby
+include vendor/oneplus/dolby/BoardConfigDolby.mk
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
